4cf748b2b5acf567225349d915f8cb6e6fc5dce9
FEWA <ciroalegretti@NOCPU75167>
2025-02-20 08:51:03 -0300

    starting chap3

--- chap3_old/ex1.cpp
+++ chap3_old/ex1.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 	double y[4] = {0.0, 0.0, 1.0, 1.0};
 	char action;
 	bool a;
-	
+
 	std::ifstream write_out("ex1.dat"); // reading as ifstream
 
 	if (write_out.is_open())
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
 		}
 
 	write_out.close();
-	
+
 	if (action == 'o')
 		{
 		std::ofstream write_out("ex1.dat");
@@ -31,22 +31,22 @@ int main(int argc, char* argv[])
 		write_out.setf(std::ios::scientific);
 		write_out.setf(std::ios::showpos);
 		write_out.precision(10);
-		
+
 		for (int i=0; i<4; i++)
 		{
 			write_out << x[i] << "\t";
 		}
 		write_out.flush();
 		write_out << "\n";
-		
+
 		for (int i=0; i<4; i++)
 		{
 			write_out << y[i] << "\t";
 		}
 		write_out.flush();
-		write_out << "\n";		
+		write_out << "\n";
 		}
-		
+
 	else if (action == 'a')
 		{
 		std::ofstream write_out("ex1.dat", std::ios::app);
@@ -54,27 +54,27 @@ int main(int argc, char* argv[])
 		write_out.setf(std::ios::scientific);
 		write_out.setf(std::ios::showpos);
 		write_out.precision(10);
-		
+
 		for (int i=0; i<4; i++)
 		{
 			write_out << x[i] << "\t";
 		}
 		write_out.flush();
 		write_out << "\n";
-		
+
 		for (int i=0; i<4; i++)
 		{
 			write_out << y[i] << "\t";
 		}
 		write_out.flush();
-		write_out << "\n";		
-		
+		write_out << "\n";
+
 		}
 	else
 		{
 		std::cout << "Invalid option \n";
 		}
-	
+
 	write_out.close();
 
 	return 0;
---
+++ chap3_old/sampleCode/sec3ss1_sample.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+
+    int x, y;
+
+    if (y == 0)
+    {
+        std::cout << "Error - division by zero \n";
+    }
+    else
+    {
+	std::cout << x/y << "\n";
+    }
+
+    return 0;
+
+}
