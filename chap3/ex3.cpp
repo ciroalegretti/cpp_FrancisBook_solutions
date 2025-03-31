d9cb38927330d682fe4e593461c16b5cc5c99ff8
FEWA <ciroalegretti@NOCPU75167>
2025-03-31 10:32:25 -0300

    included perc error calculation for chap3/ex3

--- chap3/ex3.cpp
+++ chap3/ex3.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <cassert>
+#include <iomanip>
+#include <cmath>
 
 int main(int argc, char* argv[])
 {
@@ -9,8 +12,9 @@ int main(int argc, char* argv[])
 
     double step = 1.0/(number_of_nodes - 1);
     // arrays initialization with zero-based indexing
-    double x[number_of_nodes - 1];
-    double y[number_of_nodes - 1];
+    double x[number_of_nodes];
+    double y[number_of_nodes];
+    double y_ex[number_of_nodes];
 
     // print node count and grid step size
     std::cout << "Number of nodes = " << number_of_nodes << "\n";
@@ -18,16 +22,31 @@ int main(int argc, char* argv[])
 
     for (int i = 0; i < number_of_nodes; i++)
     {
-	x[i] += i*step;
-	std::cout << x[i] << "\n";
+	x[i] = i*step;
+	//std::cout << x[i] << "\t" << i << "\n";
     }
+
+    // enforce boundary condition
+    y[0] = 1.0;
+    y_ex[0] = 1.0;
 
     std::ofstream data_out("xy.dat");
     assert(data_out.is_open());
 
-    data_out << "x" << std::setw(25) << "y" << std::setw(30) << "y_ex" << std::setw(25) << "err (%) \n";
-    data_out << x[0] << "\t" << y[0] << "\t" << exp( - x[0]) << "\t" << (exp( - x[0]) - y[0])/exp( - x[0]) << "\n";
+    data_out.precision(4);
 
+    data_out << "x \t" << "y_calc \t" << "y_exac \t" << "err (%) \n";
+    data_out << x[0] << "\t" << y[0] << "\t" << exp( - x[0]) << "\t" << 0.0 << "\n";
+
+    for (int i = 1; i < number_of_nodes; i++)
+    {
+        y[i] = y[i-1]/(step + 1);
+        y_ex[i] = exp( - x[i]);
+	data_out << x[i] << "\t" << y[i] << "\t" << y_ex[i] << "\t" << std::abs(y_ex[i] - y[i])*100/y_ex[i] << "\n";
+    }
+
+    data_out.flush();
+    data_out.close();
 
 
     return 0;
